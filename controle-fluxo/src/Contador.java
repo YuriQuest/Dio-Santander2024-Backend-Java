9b9f4fe7d812ec9b4e0e4ca9d5c995f63a67ee2b
YuriQuest <162909475+YuriQuest@users.noreply.github.com>
2024-05-15 19:34:07 -0300

    inicio controle Fluxo

---
+++ controle-fluxo/src/Contador.java
@@ -0,0 +1,35 @@
+/**
+* <h1>Conta Terminal/h1>
+* Este programa simula uma criação de conta via terminal
+* <p>
+* <b>Note:</b> Leia atentamente a documentação da classe
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  YuriQuest (Yuri M. dos Santos rodrigues)
+* @version 1.0
+* @since   15/05/2024
+*/
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.??;
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.??;
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (? exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+	}
+}
