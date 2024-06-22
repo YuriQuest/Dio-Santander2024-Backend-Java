1d11b2c8f68c6c6258a3d0ca60b51dac6caaa50a
YuriQuest <yurimarques.el@gmail.com>
2024-06-21 21:44:00 -0300

    :sparkles: feat: Sistema basico em funcionamento

--- Santander 2024 - Backend com Java/desafio-poo/src/Main.java
+++ Santander 2024 - Backend com Java/desafio-poo/src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,40 @@ public class Main {
         mentoria.setDescricao(" descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Camila:" + devCamila.getConteudosIncritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Camila:" + devCamila.getConteudosIncritos());
+        System.out.println("Conteudos Concluídos Camila:" + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularXp());
+
+        System.out.println("--------------");
+
+        Dev devYuri = new Dev();
+        devYuri.setNome("Yuri");
+        devYuri.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Yuri:" + devYuri.getConteudosIncritos());
+        devYuri.progredir();
+        devYuri.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Yuri:" + devYuri.getConteudosIncritos());
+        System.out.println("Conteudos Concluídos Yuri:" + devYuri.getConteudosConcluidos());
+        System.out.println("XP:" + devYuri.calcularXp());
+
+
     }
 }
